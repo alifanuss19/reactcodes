c368efad23e367656b61fb3c09ebbcd20ddc9cc4
alifanuss <56692668+alifanuss19@users.noreply.github.com>
2022-05-10 18:30:24 +0430

    Update Timefunchoo.js

--- Timefunchoo.js
+++ Timefunchoo.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 const Timefunchoo = () => {
   const [timeis, setTimeis] = useState(new Date().toLocaleTimeString());
+  useEffect(() => {
+    const timesID = setInterval(times, 1000);
+    return () => {
+      clearInterval(timesID);
+    }
+  }, [timeis]);
   const times = () => {
     setTimeis(new Date().toLocaleTimeString());
-    //clearInterval(timesID);
   };
-  //const timesID = setInterval(times, 1000);
   return (
     <div onClick={times}>time is
       {' ' + timeis}
