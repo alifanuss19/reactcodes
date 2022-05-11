d7c14819405ae77733c91c612844c486bf5efa23
alifanuss <56692668+alifanuss19@users.noreply.github.com>
2022-05-12 01:21:08 +0430

    Update Formfunchoo.js

--- Formfunchoo.js
+++ Formfunchoo.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+//three & perfect
+import { useForm } from 'react-hook-form';
 const Formfunchoo = () => {
   //one & bad
   // const [name, setName] = useState("");
@@ -9,25 +11,43 @@ const Formfunchoo = () => {
   //   event.preventDefault();
   // };
   //two & good
-  const subMit = event => {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    console.log(form.get("name"));
+  // const subMit = event => {
+  //   event.preventDefault();
+  //   const form = new FormData(event.target);
+  //   console.log(form.get("name"));
+  // };
+  //three & perfect
+  const {register,handleSubmit,watch,formState:{errors},} = useForm(
+    {defaultValues:{yname:"your name"}}
+  );
+  const subMithandle = (sdata) => {
+    console.log(sdata);
+    console.log(watch("yname"));
   };
   return (
     <>
       <form onSubmit={
         //one & bad
-        //subData
+        // subData
         //two & good
-        subMit
+        // subMit
+        //three & perfect
+        handleSubmit(subMithandle)
       }>
         <label>
           name:
           {/* one & bad */}
           {/* <input name="name" type="text" value={name} onChange={chName} /> */}
           {/* two & good */}
-          <input name="name" type="text" />
+          {/* <input name="name" type="text" /> */}
+          {/* three & perfect */}
+          <input {...register(
+            "yname",{required:true,minLenght:5,pattern:/\d+/
+            })}
+          />
+          {errors.yname&&<small>please,fill</small>}
+          {errors.yname&&<small>please,fill with 5 charachter</small>}
+          {errors.yname&&<small>please,fill with numbers</small>}
         </label>
         {/* <input name="submit" type="submit" value="submit" /> */}
         <button type="submit">submit</button>
