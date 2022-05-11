8b299814780f3e0b7c3b214d8067fa81f28173f9
alifanuss <56692668+alifanuss19@users.noreply.github.com>
2022-05-11 22:15:18 +0430

    Update Formfunchoo.js

--- Formfunchoo.js
+++ Formfunchoo.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 const Formfunchoo = () => {
-  const [name, setName] = useState("");
-  const chName = (event) => {
-    setName(event.target.value.toUpperCase());
-  };
-  const subData = (event) => {
+  //one & bad
+  // const [name, setName] = useState("");
+  // const chName = (event) => {
+  //   setName(event.target.value.toUpperCase());
+  // };
+  // const subData = (event) => {
+  //   event.preventDefault();
+  // };
+  //two & good
+  const subMit = event => {
     event.preventDefault();
+    const form = new FormData(event.target);
+    console.log(form.get("name"));
   };
   return (
     <>
-      <form onSubmit={subData}>
+      <form onSubmit={
+        //one & bad
+        //subData
+        //two & good
+        subMit
+      }>
         <label>
           name:
-          <input name="name" type="text" value={name} onChange={chName} />
+          {/* one & bad */}
+          {/* <input name="name" type="text" value={name} onChange={chName} /> */}
+          {/* two & good */}
+          <input name="name" type="text" />
         </label>
-        <input type="submit" value="submit" />
+        {/* <input name="submit" type="submit" value="submit" /> */}
+        <button type="submit">submit</button>
       </form>
     </>
   );
