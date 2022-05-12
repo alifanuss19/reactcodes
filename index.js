67c69efb1c651451b416b707e98486d6aae0d378
alifanuss <56692668+alifanuss19@users.noreply.github.com>
2022-05-13 01:03:45 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import './index.css';
+import Layout from './Layouts';
+import App from './App';
+import Formclass from './Formclass';
+import Formfunchoo from './Formfunchoo';
+import Timeclass from './Timeclass';
+import Timefunchoo from './Timefunchoo';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Layout>
+      </Layout>
+      <Routes>
+        <Route path="/" exact="true" element={<App/>}/>
+        <Route path="/Formclass" element={<Formclass/>}/>
+        <Route path="/Formfunchoo" element={<Formfunchoo/>}/>
+        <Route path="/Timeclass" element={<Timeclass/>}/>
+        <Route path="/Timefunchoo" element={<Timefunchoo/>}/>
+        <Route path="*" element={<App/>}/>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
