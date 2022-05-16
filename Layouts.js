46f5972e39f9e9de46d54af3c8ef16a244874cef
alifanuss <56692668+alifanuss19@users.noreply.github.com>
2022-05-16 21:05:55 +0430

    Update Layouts.js

--- Layouts.js
+++ Layouts.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 import PropTypes from 'prop-types';
 import Links from './Links';
 
 const Layouts = (props) => {
 
-  const {children,userInfo}=props;
+  
+  const location = useLocation();
+  
+  const userInfo=JSON.parse(localStorage.getItem('user'));
+
+  const gologOut=()=>{
+    if(userInfo){
+    localStorage.clear();
+    window.location.reload();
+    };
+  };
+
+  const {children}=props;
 
   return (
 
@@ -13,6 +26,9 @@ const Layouts = (props) => {
       {userInfo != null ? userInfo.userName : null}
     </div>
       <Links/>
+    <div>
+    <button onClick={gologOut}>logOut</button>
+    </div>
     <div>
       {children}
     </div> 
